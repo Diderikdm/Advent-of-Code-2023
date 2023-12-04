0bb2132649f3c19e6eabeff185620df08090dc24
Diderik <diderik.demos@ad.dfnld.nl>
2023-12-04 14:24:31 +0100

    day 04

--- day04.py
+++ day04.py
@@ -1,15 +1,15 @@
 with open("day04.txt") as file:
     data = file.read().splitlines()
-    cards, cards_p2 = set(), {}
-    r1 = 0
+    r1, cards = 0, {}
     for row in data:
         card, numbers = row.split(":")
-        cards.add((card := int(card.split()[1].strip())))
-        cards_p2[card] = cards_p2.get(card, 0) + 1
+        cards[card] = cards.get((card := int(card.split()[1].strip())), 0) + 1
         winning, own = numbers.split("|")
         if (overlap := {int(x) for x in winning.split() if x} & {int(x) for x in own.split() if x}):
             r1 += (2 ** (len(overlap) - 1))
             for c in range(card + 1, card + len(overlap) + 1):
-                cards_p2[c] = cards_p2.get(c, 0) + cards_p2[card]
-    r2 = sum(v for k, v in cards_p2.items() if k in cards)
-    print(f"p1: {r1}, p2: {r2}")
+                cards[c] = cards.get(c, 0) + cards[card]
+    print(f"p1: {r1}, p2: {sum(cards.values())}")
+
+# Oneliner
+print((r1,sum(c))if all([not(r1:=0),(c:=[1]*len(data)),(y:=lambda s:{int(x)for x in s.split()if x}),[x for e,x in enumerate(open("day04.txt").read().splitlines())if(o:=y((w:=x.split(":")[1].split("|"))[0])&y(w[1]))and(r1:=r1+(2**(len(o)-1)))and[c.insert(d,c.pop(d)+1*c[e])for d in range(e+1,e+len(o)+1)]]])else 0)
