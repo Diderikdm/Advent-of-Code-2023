a3038abfdd3949d0aed3cf72ad686079e0758084
Diderik <diderik.demos@ad.dfnld.nl>
2023-12-04 17:37:15 +0100

    day 04

--- day04.py
+++ day04.py
@@ -1,12 +1,13 @@
 with open("day04.txt") as file:
     data = file.read().splitlines()
-    r1, cards = 0, {}
+    r1, r2, cards = 0, 0, {}
     for row in data:
         card, numbers = row.split(":")
         cards[card] = cards.get((card := int(card.split()[1].strip())), 0) + 1
         winning, own = numbers.split("|")
         if (overlap := {int(x) for x in winning.split() if x} & {int(x) for x in own.split() if x}):
             r1 += (2 ** (len(overlap) - 1))
+            r2 += 1 << sum(row.split().count(x) == 2 for x in row.split() if x) // 2 >> 1
             for c in range(card + 1, card + len(overlap) + 1):
                 cards[c] = cards.get(c, 0) + cards[card]
     print(f"p1: {r1}, p2: {sum(cards.values())}")
@@ -17,3 +18,17 @@ x)for x in s.split()if x}),[x for e,x in enumerate(open("day04.txt").read(
 ).splitlines())if(o:=y((w:=x.split(":")[1].split("|"))[0])&y(w[1]))and(
 r1:=r1+(2**(len(o)-1)))and[c.insert(d,c.pop(d)+1*c[e])for d in range(
 e+1,e+len(o)+1)]]])else 0)
+
+# Less efficient (double .split())
+a, b = 0, [1] * len(data := open("day04.txt", "r").read().splitlines())
+for e, line in enumerate(data):
+    a+=1<<(s:=sum(line.split().count(x)==2 for x in line.split() if x)//2)>>1
+    [b.insert(i, b.pop(i)+1 * b[e]) for i in range(e+1, e+s+1)]
+print(a, sum(b))
+
+# Oneline
+print((a,sum(b))if not(a:=0)and(b:=[1]*len(c:=open("day04.txt","r").read(
+).splitlines()))and[x for e,x in enumerate(c)if(a:=a+(1<<(s:=sum(x.split(
+).count(y)== 2 for y in x.split()if y)//2)>>1))and[b.insert(d,b.pop(d)+1*b[e]
+)for d in range(e+1,e+s+1)]]else 0)
+
