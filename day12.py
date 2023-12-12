3aa0f0771b7b31f09b42abc1cc7d074e93d16966
Diderik <diderik.demos@ad.dfnld.nl>
2023-12-12 17:38:42 +0100

    day 12

---
+++ day12.py
@@ -0,0 +1,21 @@
+from functools import cache
+
+@cache
+def recurse(lava, springs, result=0):
+    if not springs:
+        return '#' not in lava
+    current, springs = springs[0], springs[1:]
+    for i in range(len(lava) - sum(springs) - len(springs) - current + 1):
+        if "#" in lava[:i]:
+            break
+        if (nxt := i + current) < len(lava) + 1 and '.' not in lava[i : nxt] and lava[nxt : nxt + 1] != "#":
+            result += recurse(lava[nxt + 1:], springs)
+    return result
+
+with open("day12.txt", "r") as file:
+    data = [x.split() for x in file.read().splitlines()]
+    p1, p2 = {}, {}
+    for e, (lava, springs) in enumerate(data):
+        p1[e] = recurse(lava, (springs := tuple(map(int, springs.split(",")))))
+        p2[e] = recurse("?".join([lava] * 5), springs * 5)
+    print(sum(x for x in p1.values()), sum(x for x in p2.values()))
