ef6787c1d684c82d61752eb3e48ccd311cf9701b
Diderik <diderik.demos@ad.dfnld.nl>
2023-12-13 12:36:39 +0100

    day 13

--- day13.py
+++ day13.py
@@ -1,3 +1,22 @@
+def calculate_reflection(p1, p2, y=-1):
+    if e and len([u for u in range(len(d[e])) if d[e][u] != d[e - 1][u]]) < 2:
+        smudge = False
+        for i in range(e, len(d) + 1):
+            y += 2
+            if i - y < 0 or i == len(d):
+                if smudge: p2 += e * multiplier
+                else:      p1 += e * multiplier
+                break
+            if d[i] != d[i - y]:
+                if not smudge and len(a := [u for u in range(len(d[i])) if d[i][u] != d[i - y][u]]) == 1: 
+                    smudge = 1
+                else: break
+        else:
+            if smudge: p2 += e * multiplier
+            else:      p1 += e * multiplier
+    return p1, p2
+        
+
 with open("day13.txt", "r") as file:
     data = file.read().split("\n\n")
     p1 = p2 = 0
@@ -6,20 +25,5 @@ with open("day13.txt", "r") as file:
         v = [*map(list, zip(*h))]
         for d, multiplier in ((h, 100), (v, 1)):
             for e, x in enumerate(d):
-                y = -1
-                if e and len([u for u in range(len(d[e])) if d[e][u] != d[e - 1][u]]) < 2:
-                    smudge = False
-                    for i in range(e, len(d) + 1):
-                        y += 2
-                        if i - y < 0 or i == len(d):
-                            if smudge: p2 += e * multiplier
-                            else:      p1 += e * multiplier
-                            break
-                        if d[i] != d[i - y]:
-                            if not smudge and len(a := [u for u in range(len(d[i])) if d[i][u] != d[i - y][u]]) == 1: 
-                                smudge = 1
-                            else: break
-                    else:
-                        if smudge: p2 += e * multiplier
-                        else:      p1 += e * multiplier
+                p1, p2 = calculate_reflection(p1, p2)
     print(p1, p2)
