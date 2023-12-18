4f7fe4f40662e0bec758f1f7b541da20e72e0379
Diderik <diderikdm@gmail.com>
2023-12-18 16:14:25 +0100

    day 18

--- day18.py
+++ day18.py
@@ -1,7 +1,7 @@
 with open("day18.txt", "r") as file:
     data = [x.split() for x in file.read().splitlines()]
     grid_1, grid_2, current_1, current_2, steps_1, steps_2, result = [(0, 0)],[(0, 0)], (0, 0), (0, 0), 0, 0, []
-    walk = lambda n, d, x, y: {"U" : ((x, y - n)), "D" : ((x, y + n)), "L" : ((x - n, y)), "R" : ((x + n, y))}[d]
+    walk = lambda n, d, x, y: {"U" : (x, y - n), "D" : (x, y + n), "L" : (x - n, y), "R" : (x + n, y)}[d]
     for direction, steps, color in data:
         grid_1.append(current_1 := walk((steps := int(steps)), direction, *current_1)); steps_1 += steps
         grid_2.append(current_2 := walk((steps := int(color[2:-2], 16)), {"0" : "R", "1" : "D", "2" : "L", "3" : "U"}[color[-2]], *current_2)); steps_2 += steps
