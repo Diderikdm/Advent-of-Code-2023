2e6b684bfc0c9d839718c0e7dfb68824b003bbd2
Diderik <diderikdm@gmail.com>
2023-12-19 16:49:42 +0100

    day 19

--- day19.py
+++ day19.py
@@ -24,22 +24,15 @@ with open("day19.txt", "r") as file:
         workflows[label] = []
         vars = {**{x : 1 for x in "xmas"}, **{x : 4000 for x in "XMAS"}}
         for check in checks:
-            if ":" not in check: 
-                other_label, current = check, [[vars[x], vars[x.upper()]] for x in "xmas"]
+            if ":" not in check:  other_label, current = check, [[vars[x], vars[x.upper()]] for x in "xmas"]
             else:
                 assertion, other_label = check.split(":")
                 (var, value) = assertion.split(sign := ["<", ">"][">" in assertion])
                 current = []
                 for x in "xmas":
                     if x == var:
-                        if sign == ">":
-                            current += [[int(value) + 1, vars[x.upper()]]]
-                            vars[x.upper()] = int(value)
-                        elif sign == "<":
-                            current += [[vars[x], int(value) - 1]]
-                            vars[x] = int(value)
-                    else:
-                        current += [[vars[x], vars[x.upper()]]]                
+                        if   sign == ">": current += [[int(value) + 1, vars[x.upper()]]]; vars[x.upper()] = int(value)
+                        elif sign == "<": current += [[vars[x], int(value) - 1]];         vars[x] = int(value)
+                    else:                 current += [[vars[x], vars[x.upper()]]]                
             workflows[label].append((other_label, current))
-    
-    print([recurse("in", *[[1, 4000]] * 4), sum(sum(x) for x in p1)][::-1])
+    print([recurse("in", *[[1,4000]] * 4), sum(sum(x) for x in p1)][::-1])
