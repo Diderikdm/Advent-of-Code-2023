6abed0499a3cda6f96cfdf3ddb2d355776812051
Diderik <diderikdm@gmail.com>
2023-12-25 15:19:56 +0100

    day25

--- day25.py
+++ day25.py
@@ -3,8 +3,8 @@ import networkx as nx
 with open("day25.txt", "r") as file:
     graph = nx.Graph()
     for row in file.read().splitlines():
-        left, right = row.split(': ')
-        for other in right.split(' '):
-            graph.add_edge(left, other)
+        node, others = row.split(': ')
+        for other in others.split(' '):
+            graph.add_edge(node, other)
     bisected = nx.spectral_bisection(graph)
     print(len(bisected[0]) * len(bisected[1]))
